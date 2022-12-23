3bdb15d036f29b1630294759341a3974f2088dc3
Roman <mazaxaka.tyt@gmail.com>
2022-12-23 13:08:52 +0200

    split slider in two

--- app.js
+++ app.js
@@ -103,8 +103,47 @@ window.addEventListener("scroll", function () {
 });
 
 // Slider
+// $(document).ready(function () {
+//   $(".slider").slick({
+//     arrows: false,
+//     infinite: true,
+//     dots: false,
+//     variableWidth: true,
+//     slidesToShow: 3,
+//     centerMode: true,
+//     centerPadding: "60px",
+//     responsive: [
+//       {
+//         breakpoint: 576,
+//         settings: {
+//           centerMode: false,
+//         },
+//       },
+//     ],
+//   });
+// });
+
 $(document).ready(function () {
-  $(".slider").slick({
+  $(".slider-video").slick({
+    arrows: false,
+    infinite: true,
+    dots: false,
+    variableWidth: true,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: "60px",
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          centerMode: false,
+        },
+      },
+    ],
+  });
+});
+$(document).ready(function () {
+  $(".slider-text").slick({
     arrows: false,
     infinite: true,
     dots: false,
